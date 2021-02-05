0f45cb6795ad897b7f1c23b64caf32183ca83c90
Michael Xiang <yorkmike4@gmail.com>
2021-02-05 09:18:21 +0000

    feature/about-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+
+  get 'about' => 'about#index'
 
   resource :cart, only: [:show] do
     post   :add_item
