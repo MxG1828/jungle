0d385681573fbbe393e2c581cb5fedcacb5e24e3
Michael Xiang <yorkmike4@gmail.com>
2021-02-09 12:02:09 +0000

    maybe pull user auth

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  describe 'Validations' do
+    # it 'able to create new user' do
+    #   @user = User.new(
+    #     :email=>"123@321.com"
+    #     :password=>
+    #   )
+    #   @product.save
+    #   expect(@product).to be_present
+    # end
+  end
+end
