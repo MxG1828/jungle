d16a38ae7c1bf9590defa3cb2482d6f6c4545eca
Michael Xiang <yorkmike4@gmail.com>
2021-02-10 03:06:39 +0000

    validates user regester

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :email
+  validates :email, uniqueness: { case_sensitive: false }
+  validates_length_of :password, minimum:6
+
+  
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,14 +1,70 @@
 require 'rails_helper'
 
-RSpec.feature "Users", type: :feature do
-  describe 'Validations' do
-    # it 'able to create new user' do
-    #   @user = User.new(
-    #     :email=>"123@321.com"
-    #     :password=>
-    #   )
-    #   @product.save
-    #   expect(@product).to be_present
-    # end
+
+describe 'Validations' do
+  
+  it 'create new user' do
+    @user = User.new(
+      :email=>"1@1.com",
+      :password=>"123123",
+      :password_confirmation=>"123123"
+    )
+    expect(@user).to be_valid
   end
+  it 'must have email' do
+    @user = User.new(
+      :email=>"",
+      :password=>"123123",
+      :password_confirmation=>"123123"
+    )
+    expect(@user).to_not be_valid
+  end
+  it 'must have password' do
+    @user = User.new(
+      :email=>"2@2.com",
+      :password=>"",
+      :password_confirmation=>"123123"
+    )
+    expect(@user).to_not be_valid
+  end
+  it 'must have password comfirmation' do
+    @user = User.new(
+      :email=>"3@3.com",
+      :password=>"123123",
+      :password_confirmation=>""
+    )
+    expect(@user).to_not be_valid
+  end
+  it 'password and comfirmation matches' do
+    @user = User.new(
+      :email=>"4@4.com",
+      :password=>"123123",
+      :password_confirmation=>"1231233"
+    )
+    expect(@user).to_not be_valid
+  end
+  it 'password must have a minimum length of 6' do
+    @user = User.new(
+      :email=>"5@5.com",
+      :password=>"12312",
+      :password_confirmation=>"12312"
+    )
+    expect(@user).to_not be_valid
+  end
+  it 'email must be unique' do
+    @testuser = User.new(
+      :email=>"test@test.com",
+      :password=>"123123",
+      :password_confirmation=>"123123"
+    )
+    @testuser.save
+    @user = User.new(
+      :email=>"test@test.com",
+      :password=>"123123",
+      :password_confirmation=>"123123"
+    )
+    expect(@user).to_not be_valid
+  end
+
+
 end
